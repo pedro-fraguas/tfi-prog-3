627a75462d068422b2aab58ef2c73966f58cc351
pedro <pfraguas@udesa.edu.ar>
2021-03-12 17:42:42 -0300

    initial commit, proyecto base

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Prueba de React</h1>
+      <ul>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
