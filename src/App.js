31027d491fe6022e80063fbd9990732034105cea
Joaquin Berardi <Joaquinberardi@gmail.com>
2021-03-23 15:56:32 -0300

    index html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Prueba de React</h1>
-      <ul>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-        <li>Item</li>
-      </ul>
+      
     </div>
   );
 }
