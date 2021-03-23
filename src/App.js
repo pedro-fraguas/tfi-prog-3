4391935bd2bf479b8c4b00886f7c987c65c5e5bf
pedro <pfraguas@udesa.edu.ar>
2021-03-23 16:32:10 -0300

    update on app.js index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,70 @@
+import UserCard from './components/UserCard'
+
 function App() {
   return (
     <div className="App">
+
+      {/*<!-- HEADER -->*/}
+      <div class="header header-fixed unselectable header-animated">
+        <div class="header-brand">
+          <div class="nav-item no-hover">
+            <h6 class="title">Programacion 3</h6>
+          </div>
+          <div class="nav-item nav-btn" id="header-btn"> <span></span> <span></span> <span></span> </div>
+        </div>
+        <div class="header-nav" id="header-menu">
+          <div class="nav-left">
+            <div class="nav-item text-center">  <span class="icon"> <i class="fas fa-code"></i> </span> </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="section" style={{'background-color': '#FBF8F3;'}}>
+        <div class="hero">
+          <div class="hero-body">
+            <div class="content">
+
+              {/*<!-- BUSCADOR -->*/}
+              <div class="form-group card">
+                 <input type="search" class="form-group-input" placeholder="Filtrar por Nombre, Apellido o Edad" />
+                 <button class="form-group-btn">Buscar</button>
+              </div>
+
+              {/*<!-- AÑADIR PERSONAS -->*/}    
+              <div class="form-group card">
+                 <input type="number" class="input-contains-icon"  placeholder="Agregar x Cantidad de personas" />
+                 <button class="form-group-btn">Añadir</button>
+              </div>
+
+              {/*<!-- ORDENAR -->*/}
+              <div class="input-control card">
+               <select class="select" placeholder="Filtrar por">
+                  <option value="0">Ordenar por</option>
+                  <option value="1">Nombre descendente</option>
+                  <option value="2">Nombre ascendente</option>
+                  <option value="3">Edad descendente</option>
+                  <option value="4">Edad ascendente</option>
+               </select>
+              </div>
+
+              <div class="divider"></div>
+
+
+              {/*<!-- Contenedor de tarjetas -->*/}
+              <div class="grid grid-cols-3 grid-gap-3">
+                
+                < UserCard />
+                < UserCard />
+                < UserCard />
+                < UserCard />
+                < UserCard />
+                < UserCard />
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       
     </div>
   );
