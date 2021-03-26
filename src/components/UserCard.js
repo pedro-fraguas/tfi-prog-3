260c590907abea70c6df36b50140ad8b39bdab17
pedro <pfraguas@udesa.edu.ar>
2021-03-26 19:17:53 -0300

    added future api response

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,21 +2,42 @@ function UserCard(props) {
     return(
         <div class="card u-flex u-flex-column">
             <div class="card__container">
-                  <div class="card__image" style={{"background-image": "url(https://camo.githubusercontent.com/eb6a385e0a1f0f787d72c0b0e0275bc4516a261b96a749f1cd1aa4cb8736daba/68747470733a2f2f612e736c61636b2d656467652e636f6d2f64663130642f696d672f617661746172732f6176615f303032322d3531322e706e67);"}}></div>
+                <div class="card__image" style={{"background-image": "url(https://camo.githubusercontent.com/eb6a385e0a1f0f787d72c0b0e0275bc4516a261b96a749f1cd1aa4cb8736daba/68747470733a2f2f612e736c61636b2d656467652e636f6d2f64663130642f696d672f617661746172732f6176615f303032322d3531322e706e67);"}}></div>
             </div>
         
             <div class="content">
-               <p class="title">{props.name}</p>
-                  <p>Edad: {props.age}</p>
-                  <p>{props.email}</p>
+                <p class="title">{props.name}</p>
+                <p>Edad: {props.age}</p>
+                <p>{props.email}</p>
             </div>
         
             <div class="card__action-bar u-center">
-                  <button class="btn-transparent ">Ver detalle <i class="fas fa-angle-right"></i></button>
-                  <button class="btn-transparent ">Descartar <i class="far fa-trash-alt"></i></button>
+                <button class="btn-transparent ">Ver detalle <i class="fas fa-angle-right"></i></button>
+                <button class="btn-transparent ">Descartar <i class="far fa-trash-alt"></i></button>
             </div>
                   
         </div>
+
+        /**
+         * <div class="card u-flex u-flex-column">
+            <div class="card__container">
+                <div class="card__image" style={{"background-image": "url(" + props.user.picture.medium + ")"}}></div>
+            </div>
+        
+            <div class="content">
+                <p class="title">{props.user.name.first} {props.user.name.last}</p>
+                <p>Edad: {props.user.dob.age}</p>
+                <p>{props.user.email}</p>
+            </div>
+        
+            <div class="card__action-bar u-center">
+                <button class="btn-transparent ">Ver detalle <i class="fas fa-angle-right"></i></button>
+                <button class="btn-transparent ">Descartar <i class="far fa-trash-alt"></i></button>
+            </div>
+                  
+        </div>
+         */
+
     )
 }
 
