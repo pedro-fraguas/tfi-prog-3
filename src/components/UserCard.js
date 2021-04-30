c5c28add0839a9ffd57b68d23b7cd7eb07f82194
pedro <pfraguas@udesa.edu.ar>
2021-04-30 11:11:30 -0300

    merge

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -20,27 +20,6 @@ class UserCard extends Component {
                     <div className="card__image" style={{"backgroundImage": "url(" + this.props.specs.picture.large + ")"}}></div>
                 </div>
 
-                {/**<div class="card__mobile-title">
-                    <div class="content">
-                        <div class="tile">
-                            <div class="tile__container">
-                                <p class="tile__title">{this.props.specs.name.first} {this.props.specs.name.last} ({this.props.specs.dob.age})</p>
-                                <p class="tile__subtitle">{this.props.specs.location.country}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card__body content">
-                    <p>Email: {this.props.specs.email}</p>
-                    <p>{this.props.specs.dob.date}</p>
-                </div>
-                <div class="card__footer content">Gender: {this.props.specs.gender}</div>
-
-                <div className="card__action-bar u-center">
-                    <button className="btn-transparent "> Ver detalle <i className="fas fa-angle-right"></i></button>
-                    <button className="btn-transparent " onClick={() => this.props.onDelete(this.props.specs.login.uuid)}> Descartar <i className="far fa-trash-alt"></i></button>
-                </div>**/}
-
                 <div className="content">
                     <p className="title">{this.props.specs.name.first} {this.props.specs.name.last} ({this.props.specs.dob.age})</p>
                     <p>Email: {this.props.specs.email}</p>
